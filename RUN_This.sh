aba09ca5ae11d61a75ee5b1ead0613639314dd51
Big-Yee <129009091+Big-Yee@users.noreply.github.com>
2023-10-03 14:55:29 +0100

    Update RUN_This.sh

--- RUN_This.sh
+++ RUN_This.sh
@@ -23,7 +23,6 @@ printf "Identifying system configuration this may take ${RED}some${NC} time...\r
 IDENT=$(system_profiler SPSoftwareDataType SPHardwareDataType | grep "Model Identifier")
 printf "Identifying system configuration this may take ${RED}some${NC} time....\r"
 CPU=$(sysctl -n machdep.cpu.brand_string)
-printf "${RED}CPU Core(s)${NC}\n"
 CPU_Cores=$(sysctl -n hw.ncpu)
 printf "Identifying system configuration this may take ${RED}some${NC} time.....\r"
 
@@ -54,6 +53,7 @@ sleep .5
 printf "${RED}System Information;${NC}\n"
 echo $IDENT
 echo $CPU
+printf "${RED}CPU Core(s)${NC}\n"
 echo $CPU_Cores
 echo $MEM
 printf "${RED}Storage;${NC}\n"
