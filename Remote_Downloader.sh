d87a8f5fae097b8d44d998141b7c04fb2b058a00
Big-Yee <129009091+Big-Yee@users.noreply.github.com>
2023-10-05 12:02:44 +0100

    Update Remote_Downloader.sh

--- Remote_Downloader.sh
+++ Remote_Downloader.sh
@@ -1,2 +1,2 @@
 #Download and Execute
-bash < <(curl -s https://raw.githubusercontent.com/Big-Yee/Mac-System-Info-Viewer/Windows-System-Information-Viewer/RUN_This.sh)
+curl -s https://raw.githubusercontent.com/Big-Yee/Mac-System-Info-Viewer/Windows-System-Information-Viewer/RUN_This.bat
