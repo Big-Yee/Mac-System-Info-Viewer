92c50c2a4f9909c22069bff1d675afeeab01b4ee
Big-Yee <129009091+Big-Yee@users.noreply.github.com>
2023-12-13 15:42:56 +0000

    Update RUN_This.sh
    Added Eula
    
    Added Bert

--- RUN_This.sh
+++ RUN_This.sh
@@ -12,6 +12,59 @@ DISP="NULL"
 CYC="NULL"
 INPUT="NULL"
 
+printf "The following script will identify the configuration of the system it is running on. \nIn our testing we have found this to be accurate, however there is always the possibility that this information is not correct and advise that you use your judgement to determine if the data presented to you is accurate. \n"
+printf "This script remains the whole property of and was developed by BigYee Ltd. T/A Palland and Nine Security. \nHOWEVER as agreed by our Director the source for this script will remain open source and ANY business that wishes to use this is able to without cost aside from the worked hours of the staff maintaining this should they wish for us to add features to this scrript. \n"
+printf "This is to be paid at or above the National Living Wage (NLW) for the United Kindgom and Northern Ireland in compliance with international Modern Slavery legislation. We reserve the right to request further compensation should we deem this appropriate. \n"
+printf "Should you encouter an error with this script or the data reported please submit a bug report here; https://github.com/Big-Yee/Mac-System-Info-Viewer/. The same link can be used to submit feature requests. \n"
+printf "\n"
+
+sleep 1
+
+echo -n''
+echo $''
+sleep 0.05
+echo $'      _,.'
+sleep 0.05
+echo $'    ,` -.)'
+sleep 0.05
+echo $'   \'( _/\'-\\\\-.'
+sleep 0.05
+echo $'  /,|`--._,-^|          ,'
+sleep 0.05
+echo $'  \\_| |`-._/||          ,\'|'
+sleep 0.05
+echo $'    |  `-, / |         /  /'
+sleep 0.05
+echo $'    |     || |        /  /'
+sleep 0.05
+echo $'     `r-._||/   __   /  /'
+sleep 0.05
+echo $' __,-<_     )`-/  `./  /'
+sleep 0.05
+echo $'\'  \\   `---\'   \\   /  /'
+sleep 0.05
+echo $'    |           |./  /'
+sleep 0.05
+echo $'    /           //  /'
+sleep 0.05
+echo $'\\_/\' \\         |/  /'
+sleep 0.05
+echo $' |    |   _,^-\'/  /'
+sleep 0.05
+echo $' |    , ``  (\\/  /_'
+sleep 0.05
+echo $'  \\,.->._    \\X-=/^'
+sleep 0.05
+echo $'  (  /   `-._//^`'
+sleep 0.05
+echo $'   `Y-.____(__}'
+sleep 0.05
+echo $'    |     {__)'
+sleep 0.05
+echo $'          ()`'
+sleep 0.05
+echo ''
+
 #Fancy Colours and Shit
 RED="\033[0;31m"
 NC="\033[0m"
