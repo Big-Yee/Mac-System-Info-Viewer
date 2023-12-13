da9e1082e61f0efcbc7c6c3ef7f67aa5ae8edd71
Big-Yee <129009091+Big-Yee@users.noreply.github.com>
2023-12-13 15:34:48 +0000

    Update RUN_This.sh

--- RUN_This.sh
+++ RUN_This.sh
@@ -15,11 +15,11 @@ HEALTH="NULL"
 CYC="NULL"
 INPUT="NULL"
 
-echo "The following script will identify the configuration of the system it is running on. \nIn our testing we have found this to be accurate, however there is always the possibility that this information is not correct and advise that you use your judgement to determine if the data presented to you is accurate. \n"
-echo "This script remains the whole property pf and was developed by BigYee Ltd. T/A Palland and Nine Security. \nHOWEVER as agreed by our Director the source for this script will remain open source and ANY business that wishes to use this is able to without cost aside from the worked hours of the staff maintaining this should they wish for us to add features to this scrript. \n"
-echo "This is to be paid at or above the National Living Wage (NLW) for the United Kindgom and Northern Ireland in compliance with international Modern Slavery legislation. We reserve the right to request further compensation should we deem this appropriate. \n"
-echo "Should you encouter an error with this script or the data reported please submit a bug report here; https://github.com/Big-Yee/Mac-System-Info-Viewer/. The same link can be used to submit feature requests. \n"
-echo "\n"
+printf "The following script will identify the configuration of the system it is running on. \nIn our testing we have found this to be accurate, however there is always the possibility that this information is not correct and advise that you use your judgement to determine if the data presented to you is accurate. \n"
+printf "This script remains the whole property pf and was developed by BigYee Ltd. T/A Palland and Nine Security. \nHOWEVER as agreed by our Director the source for this script will remain open source and ANY business that wishes to use this is able to without cost aside from the worked hours of the staff maintaining this should they wish for us to add features to this scrript. \n"
+printf "This is to be paid at or above the National Living Wage (NLW) for the United Kindgom and Northern Ireland in compliance with international Modern Slavery legislation. We reserve the right to request further compensation should we deem this appropriate. \n"
+printf "Should you encouter an error with this script or the data reported please submit a bug report here; https://github.com/Big-Yee/Mac-System-Info-Viewer/. The same link can be used to submit feature requests. \n"
+printf "\n"
 
 sleep 3
 
